f99f0902e8a17f4de72135d9de00c30961ed964b
leahdizon <gejing199278er>
2022-01-26 16:36:05 +0800

    deploy

---
+++ precache-manifest.e491ecc72d53bf33a90da56dc8d3ee23.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19548ece6b038157cc5d",
+    "url": "/courier-personal-websit/css/address-confirm.95dd9f4b.css"
+  },
+  {
+    "revision": "3db3918bb48fbbcfbbe1",
+    "url": "/courier-personal-websit/css/app.9000ed95.css"
+  },
+  {
+    "revision": "51279778981ec78854d7",
+    "url": "/courier-personal-websit/css/certification-result.c5ac4383.css"
+  },
+  {
+    "revision": "68a86e00536cf249c053",
+    "url": "/courier-personal-websit/css/certification.12094878.css"
+  },
+  {
+    "revision": "062abfa56032c1c6b602",
+    "url": "/courier-personal-websit/css/chunk-vendors.366b9c75.css"
+  },
+  {
+    "revision": "18e7076594755d770dcf",
+    "url": "/courier-personal-websit/css/goods-info.5ea9b688.css"
+  },
+  {
+    "revision": "4cd36b51c2b196da4c39",
+    "url": "/courier-personal-websit/css/login.21ecdba8.css"
+  },
+  {
+    "revision": "68dff1cbe7ec56bcc44b",
+    "url": "/courier-personal-websit/css/order-confirm.6695fc33.css"
+  },
+  {
+    "revision": "ef1b8940f41b29373b63",
+    "url": "/courier-personal-websit/css/order-detail.3ffdbb7f.css"
+  },
+  {
+    "revision": "76b8549c48866f3d9635",
+    "url": "/courier-personal-websit/css/order.c488fdd3.css"
+  },
+  {
+    "revision": "70fd443332590f0435cb",
+    "url": "/courier-personal-websit/css/sender-info.b23d1bca.css"
+  },
+  {
+    "revision": "5b6c3f01093717a6d5c8fdd8f3a983dd",
+    "url": "/courier-personal-websit/img/empty.5b6c3f01.png"
+  },
+  {
+    "revision": "da9b3ac3d96d715bffcb6138005cf081",
+    "url": "/courier-personal-websit/img/home-bg.da9b3ac3.png"
+  },
+  {
+    "revision": "75cf99b9991c8de6627a6013007c8e17",
+    "url": "/courier-personal-websit/img/login-bg.75cf99b9.png"
+  },
+  {
+    "revision": "61b64790eab25469bd2142d7dce12d1d",
+    "url": "/courier-personal-websit/img/phone-order.61b64790.png"
+  },
+  {
+    "revision": "9aceac5ea3ec60177a5227f3b4133310",
+    "url": "/courier-personal-websit/index.html"
+  },
+  {
+    "revision": "19548ece6b038157cc5d",
+    "url": "/courier-personal-websit/js/address-confirm.3721e179.js"
+  },
+  {
+    "revision": "3db3918bb48fbbcfbbe1",
+    "url": "/courier-personal-websit/js/app.d4bff17b.js"
+  },
+  {
+    "revision": "51279778981ec78854d7",
+    "url": "/courier-personal-websit/js/certification-result.710bb817.js"
+  },
+  {
+    "revision": "68a86e00536cf249c053",
+    "url": "/courier-personal-websit/js/certification.3b2bb43e.js"
+  },
+  {
+    "revision": "062abfa56032c1c6b602",
+    "url": "/courier-personal-websit/js/chunk-vendors.3423ca01.js"
+  },
+  {
+    "revision": "18e7076594755d770dcf",
+    "url": "/courier-personal-websit/js/goods-info.90303cc5.js"
+  },
+  {
+    "revision": "4cd36b51c2b196da4c39",
+    "url": "/courier-personal-websit/js/login.3b9347d4.js"
+  },
+  {
+    "revision": "68dff1cbe7ec56bcc44b",
+    "url": "/courier-personal-websit/js/order-confirm.dd8bf881.js"
+  },
+  {
+    "revision": "ef1b8940f41b29373b63",
+    "url": "/courier-personal-websit/js/order-detail.4168344d.js"
+  },
+  {
+    "revision": "76b8549c48866f3d9635",
+    "url": "/courier-personal-websit/js/order.64b819df.js"
+  },
+  {
+    "revision": "70fd443332590f0435cb",
+    "url": "/courier-personal-websit/js/sender-info.148dd5a1.js"
+  },
+  {
+    "revision": "dbf772b96b55c732539200383ed7edbe",
+    "url": "/courier-personal-websit/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/courier-personal-websit/robots.txt"
+  }
+]);
